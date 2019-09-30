20a6724416a77479fed8556d46f7d0205dd27a20
Micah Oltmann <micah.oltmann@gmail.com>
2019-09-30 19:08:12 -0400

    some code if fire

--- main.c
+++ main.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_hangman(int tries left);
+
+int main(void) {
+
+    char *dict[5];
+    char [6] guess;
+    dict[0] = "Cake is a lie";
+    dict[1] = "This statement is false - Glados";
+    dict[2] = "bug = undocumented feature";
+    dict[3] = "I'm a potato";
+    dict[4] = "Are you still there?";
+
+    int phrase_num = rand()%(4-0 + 1) + 0;
+    int phrase_size = sizeof(dict[phrase_num])/ sizeof(char);
+
+    char [phrase_size] phrase_guess;
+
+    printf("Welcome to Hangman!");
+    printf("Please guess a letter! If you would like to guess the phrase type \"Phrase\" followed by enter and you will be prompted for your guess!");
+
+    scanf("%s", guess);
+    if (guess == "Phrase") {
+        scanf
+    }
+
+}
 
 void print_hangman(int tries_left) {
     if (tries_left > 7) {
