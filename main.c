9cad49c2d2e3cb10ede4853bd4e2e6724db74317
Micah Oltmann <micah.oltmann@gmail.com>
2019-09-30 19:40:44 -0400

    more code if fire

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 
 void print_hangman(int tries left);
+char* guess_fill_in(char[] dictIn, char[] guess);
+void autofill_in(char* fill_in);
 
 int main(void) {
 
@@ -16,22 +18,59 @@ int main(void) {
 
     int phrase_num = rand()%(4-0 + 1) + 0;
     int phrase_size = sizeof(dict[phrase_num])/ sizeof(char);
-
     char [phrase_size] phrase_guess;
+    int tries = 7;
+    int pass = 0;
+
+    char [phrase_size] fill_in;
+    fill_in = autofill_in(fill_in);
 
     printf("Welcome to Hangman!");
     printf("Please guess a letter! If you would like to guess the phrase type \"Phrase\" followed by enter and you will be prompted for your guess!");
 
     scanf("%s", guess);
     if (guess == "Phrase") {
-        scanf
+        scanf("%s", phrase_guess);
+        int cmp = strcmp(dict[phrase_num], phrase_guess);
+        if (cmp != 0){
+
+            tries -= 3;
+            printf("You're not a good person. You know that, right? [-3 tries]" + tries + "remaining.");
+            print_hangman(tries);
+            
+            for (int i = 0; i < phrase_size; i++) {
+                for (int j = 0; j < phrase_size; j++) {
+
+
+
+                }
+            }
+
+        }
+
+
+
+    } else if() {
+        // Letter guess
+    } else {
+        // error
     }
+
+}
+// this fills in the characters from the phrase
+char* guess_fill_in(char[] dictIn, char[] guess) {
+
+
+}
+
+// This fills in all Punctuation or Spaces
+void autofill_in(char* fill_in) {
 
 }
 
 void print_hangman(int tries_left) {
     if (tries_left > 7) {
-        return;
+        tries_left = 7;
     }
     switch (tries_left) {
         case 7:
