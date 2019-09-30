81633e0afe7d83f2d798c74c56f043b491691954
Eric Pereira <ericmpereira02@gmail.com>
2019-09-30 18:39:15 -0400

    added hanging man capabilities

--- main.c
+++ main.c
@@ -1,1 +1,73 @@
+#include <stdio.h>
 
+void print_hangman(int tries_left) {
+    if (tries_left > 7) {
+        return;
+    }
+    switch (tries_left) {
+        case 7:
+            puts("┏━━━╤━");
+            puts("┃┃    ");
+            puts("┃┃");
+            puts("┃┃");
+            puts("┃┃");
+            puts("┻┻━━━━━━━");
+            break;
+        case 6:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃");
+            puts("┃┃");
+            puts("┃┃");
+            puts("┻┻━━━━━━━");
+            break;
+        case 5:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃  | ");
+            puts("┃┃    ");
+            puts("┃┃    ");
+            puts("┻┻━━━━━━━");
+            break;
+        case 4:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃ ╲| ");
+            puts("┃┃    ");
+            puts("┃┃    ");
+            puts("┻┻━━━━━━━");
+            break;
+        case 3:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃ ╲|╱");
+            puts("┃┃    ");
+            puts("┃┃    ");
+            puts("┻┻━━━━━━━");
+            break;
+        case 2:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃ ╲|╱");
+            puts("┃┃  | ");
+            puts("┃┃    ");
+            puts("┻┻━━━━━━━");
+            break;
+        case 1:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃ ╲|╱");
+            puts("┃┃  | ");
+            puts("┃┃ ╱  ");
+            puts("┻┻━━━━━━━");
+            break;
+        case 0:
+            puts("┏━━━╤━");
+            puts("┃┃  O ");
+            puts("┃┃ ╲|╱");
+            puts("┃┃  | ");
+            puts("┃┃ ╱ ╲");
+            puts("┻┻━━━━━━━");
+            break;
+    }
+}
