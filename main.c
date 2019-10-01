ddfd3527a0e24b0ed493f2c47ab775cf90c87446
Eric Pereira <ericmpereira02@gmail.com>
2019-10-01 16:13:45 -0400

    fixed some syntactical errors

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-void print_hangman(int tries left);
-char* guess_fill_in(char[] dictIn, char[] guess);
+void print_hangman(int tries_left);
+char* guess_fill_in(char dictIn[], char guess[]);
 void autofill_in(char* fill_in);
 
 int main(void) {
 
-    char *dict[5];
-    char [6] guess;
+    char dict[5];
+    char guess[6];
     dict[0] = "Cake is a lie";
     dict[1] = "This statement is false - Glados";
     dict[2] = "bug = undocumented feature";
@@ -17,13 +17,13 @@ int main(void) {
     dict[4] = "Are you still there?";
 
     int phrase_num = rand()%(4-0 + 1) + 0;
-    int phrase_size = sizeof(dict[phrase_num])/ sizeof(char);
-    char [phrase_size] phrase_guess;
+    const int phrase_size = sizeof(dict[phrase_num])/ sizeof(char);
+    char* phrase_guess = malloc(phrase_size);
     int tries = 7;
     int pass = 0;
 
-    char [phrase_size] fill_in;
-    fill_in = autofill_in(fill_in);
+    char* fill_in = malloc(phrase_size);
+    autofill_in(fill_in);
 
     printf("Welcome to Hangman!");
     printf("Please guess a letter! If you would like to guess the phrase type \"Phrase\" followed by enter and you will be prompted for your guess!");
@@ -35,7 +35,7 @@ int main(void) {
         if (cmp != 0) {
 
             tries -= 3;
-            printf("You're not a good person. You know that, right? [-3 tries]" + tries + "remaining.");
+            printf("You're not a good person. You know that, right? [-3 tries] %d remaining.", tries);
             print_hangman(tries);
 
             for (int i = 0; i < phrase_size; i++) {
@@ -48,7 +48,6 @@ int main(void) {
         }
 
 
-    }
     } else {
         printf("You guessed the char: %c", guess[0]);
         // some function to check the character
@@ -63,7 +62,7 @@ int main(void) {
 
 
 // this fills in the characters from the phrase
-char* guess_fill_in(char[] dictIn, char[] guess) {
+char* guess_fill_in(char dictIn[], char guess[]) {
 
 
 }
@@ -79,68 +78,68 @@ void print_hangman(int tries_left) {
     }
     switch (tries_left) {
         case 7:
-            puts("┏━━━╤━");
-            puts("┃┃    ");
-            puts("┃┃");
-            puts("┃┃");
-            puts("┃┃");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃    ");
+            printf("┃┃");
+            printf("┃┃");
+            printf("┃┃");
+            printf("┻┻━━━━━━━");
             break;
         case 6:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃");
-            puts("┃┃");
-            puts("┃┃");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃");
+            printf("┃┃");
+            printf("┃┃");
+            printf("┻┻━━━━━━━");
             break;
         case 5:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃  | ");
-            puts("┃┃    ");
-            puts("┃┃    ");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃  | ");
+            printf("┃┃    ");
+            printf("┃┃    ");
+            printf("┻┻━━━━━━━");
             break;
         case 4:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃ ╲| ");
-            puts("┃┃    ");
-            puts("┃┃    ");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃ ╲| ");
+            printf("┃┃    ");
+            printf("┃┃    ");
+            printf("┻┻━━━━━━━");
             break;
         case 3:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃ ╲|╱");
-            puts("┃┃    ");
-            puts("┃┃    ");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃ ╲|╱");
+            printf("┃┃    ");
+            printf("┃┃    ");
+            printf("┻┻━━━━━━━");
             break;
         case 2:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃ ╲|╱");
-            puts("┃┃  | ");
-            puts("┃┃    ");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃ ╲|╱");
+            printf("┃┃  | ");
+            printf("┃┃    ");
+            printf("┻┻━━━━━━━");
             break;
         case 1:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃ ╲|╱");
-            puts("┃┃  | ");
-            puts("┃┃ ╱  ");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃ ╲|╱");
+            printf("┃┃  | ");
+            printf("┃┃ ╱  ");
+            printf("┻┻━━━━━━━");
             break;
         case 0:
-            puts("┏━━━╤━");
-            puts("┃┃  O ");
-            puts("┃┃ ╲|╱");
-            puts("┃┃  | ");
-            puts("┃┃ ╱ ╲");
-            puts("┻┻━━━━━━━");
+            printf("┏━━━╤━");
+            printf("┃┃  O ");
+            printf("┃┃ ╲|╱");
+            printf("┃┃  | ");
+            printf("┃┃ ╱ ╲");
+            printf("┻┻━━━━━━━");
             break;
     }
 }
