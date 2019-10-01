85f7cdd1f54d436002c3e634b2c619c9c7d4a2a5
Micah Oltmann <micah.oltmann@gmail.com>
2019-10-01 16:40:32 -0400

    Not the prettiest code and not sure it works *shrug*

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 void print_hangman(int tries left);
 char* guess_fill_in(char[] dictIn, char[] guess);
 void autofill_in(char* fill_in);
+void print_fill_in(char[] fill_in);
+int check_phrase(char* fill_in, char[] dictIn, char[] guess);
 
 int main(void) {
 
@@ -18,12 +20,14 @@ int main(void) {
 
     int phrase_num = rand()%(4-0 + 1) + 0;
     int phrase_size = sizeof(dict[phrase_num])/ sizeof(char);
+    char [phrase_size] word = dict[phrase_num];
+    for (char *p = word; *p; ++p) *p = tolower(*p);
     char [phrase_size] phrase_guess;
     int tries = 7;
     int pass = 0;
 
     char [phrase_size] fill_in;
-    fill_in = autofill_in(fill_in);
+    fill_in = autofill_in(fill_in, word);
 
     printf("Welcome to Hangman!");
     printf("Please guess a letter! If you would like to guess the phrase type \"Phrase\" followed by enter and you will be prompted for your guess!");
@@ -31,24 +35,18 @@ int main(void) {
     scanf("%s", guess);
     if (guess == "Phrase") {
         scanf("%s", phrase_guess);
-        int cmp = strcmp(dict[phrase_num], phrase_guess);
+        for (char *p = phrase_guess; *p; ++p) *p = tolower(*p);
+        int cmp = check_phrase(fill_in, word, phase_guess);
         if (cmp != 0) {
-
             tries -= 3;
             printf("You're not a good person. You know that, right? [-3 tries]" + tries + "remaining.");
             print_hangman(tries);
+            print_fill_in(fill_in);
 
-            for (int i = 0; i < phrase_size; i++) {
-                for (int j = 0; j < phrase_size; j++) {
-
-
-                }
-            }
 
         }
 
 
-    }
     } else {
         printf("You guessed the char: %c", guess[0]);
         // some function to check the character
@@ -60,19 +58,61 @@ int main(void) {
 
 }
 
+// Checks if the phrase guess is complete? Not sure if needed, but ehhhhh
+int check_phrase(char* fill_in, char[] dictIn, char[] guess) {
+    int size = sizeof(dictIn)/ sizeof(char);
+    int flag = 0;
+    for (int a = 0; a < size; a++){
+        if (guess[a] != dictIn[a]) {
+            flag = 1;
+            break;
+        }
+    }
 
+    for (int a = 0; a < size; a++) {
+        guess_fill_in(fill_in, dictIn, guess[a]);
+    }
+
+    return flag;
+}
+
+// Print the blanks and filled in characters
+void print_fill_in(char[] fill_in) {
+    s = sizeof(fill_in)/sizeof(char);
+    for (int a = 0; a < s; a++) {
+        printf(fill_in[a] + " ");
+    }
+}
 
 // this fills in the characters from the phrase
-char* guess_fill_in(char[] dictIn, char[] guess) {
-
-
+char* guess_fill_in(char* fill_in, char[] dictIn, char[] guess) {
+    s = sizeof(dictIn)/sizeof(char);
+    for (int a = 0; a < s; a++) {
+        if (dictIn[a] == guess) {
+            fill_in[a] = guess;
+        }
+    }
 }
 
 // This fills in all Punctuation or Spaces
-void autofill_in(char* fill_in) {
-
+void autofill_in(char* fill_in, char[] dictIn) {
+    int phrase_size = sizeof(fill_in)/sizeof(char);
+    for (int a = 0; a < fill_in; a++) {
+        if (dictIn[a] == ' ') {
+            fill_in[a] = ' ';
+        } else if (dictIn[a] == '-') {
+            fill_in[a] = '-';
+        } else if (dictIn[a] == '?') {
+            fill_in[a] = '?';
+        }  else if (dictIn[a] == '\'') {
+            fill_in[a] = '\'';
+        }  else {
+            fill_in[a] = '_';
+        }
+    }
 }
 
+// prints the hanged man
 void print_hangman(int tries_left) {
     if (tries_left > 7) {
         tries_left = 7;
